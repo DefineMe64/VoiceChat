864f89a70e3adbf51391fbdcd3fededd7b12a741
yescco <yescco@126.com>
2020-06-06 20:13:13 +0800

    Preliminary creation of software UI layout

---
+++ VoiceChat/Chat.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VoiceChat
+{
+	/// <summary>
+	/// Chat.xaml 的交互逻辑
+	/// </summary>
+	public partial class Chat : Window
+	{
+		public Chat()
+		{
+			InitializeComponent();
+		}
+		private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			DragMove();
+		}
+		private void Close_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+		private void Min_Click(object sender, RoutedEventArgs e)
+		{
+			WindowState = WindowState.Minimized;
+		}
+	}
+}
